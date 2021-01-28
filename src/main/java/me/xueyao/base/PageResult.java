e206e444c382e5d1b8ddfa25be4963d34ae835d7
simonxue <xueyao.me@gmail.com>
2021-01-28 16:27:42 +0800

    init project

---
+++ src/main/java/me/xueyao/base/PageResult.java
@@ -0,0 +1,29 @@
+package me.xueyao.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+/**
+ * @author Simon.Xue
+ * @date 2020-03-03 20:26
+ **/
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PageResult<T> {
+    private Integer pageNum;
+    private Integer pageSize;
+    private Long count;
+    private T result;
+
+    public PageResult(T data, Page page) {
+        this.result = data;
+        this.pageNum = page.getPageable().getPageNumber() + 1;
+        this.pageSize = page.getPageable().getPageSize();
+        this.count = page.getTotalElements();
+    }
+}
